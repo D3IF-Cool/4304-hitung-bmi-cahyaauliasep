e1e0d1866b09519646dfca7a736c3b3e1d90bf5b
cahyaauliasep <cahyaauliasep@gmail.com>
2021-04-26 13:01:33 +0700

    3.2.Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if0088/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if0088/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if0088.hitungbmi.ui
+package org.d3if0088.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -13,12 +13,12 @@ import androidx.navigation.fragment.findNavController
 import org.d3if0088.hitungbmi.R
 import org.d3if0088.hitungbmi.data.KategoriBmi
 import org.d3if0088.hitungbmi.databinding.FragmentHitungBinding
+import org.d3if0088.hitungbmi.ui.HitungFragmentDirections
 
 class HitungFragment : Fragment() {
 
     private val viewModel: HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -36,6 +36,12 @@ class HitungFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections
+                    .actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
+        })
         viewModel.getHasilBmi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
             binding.bmiTextView.text = getString(R.string.bmi_x, it.bmi)
@@ -52,12 +58,8 @@ class HitungFragment : Fragment() {
         binding = FragmentHitungBinding.inflate(
             layoutInflater, container, false
         )
+        binding.saranButton.setOnClickListener { viewModel.mulaiNavigasi() }
         binding.button.setOnClickListener { hitungBmi() }
-        binding.saranButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(
-                HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
-            )
-        }
         binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
         return binding.root
--- app/src/main/java/org/d3if0088/hitungbmi/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3if0088/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -1,4 +1,4 @@
-package org.d3if0088.hitungbmi.ui
+package org.d3if0088.hitungbmi.ui.hitung
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,7 +8,10 @@ import org.d3if0088.hitungbmi.data.KategoriBmi
 
 class HitungViewModel : ViewModel(){
 
+    // Hasil BMI bisa null jika pengguna belum menghitung BMI
     private val hasilBmi = MutableLiveData<HasilBmi?>()
+    // Navigasi akan bernilai null ketika tidak bernavigasi
+    private val navigasi = MutableLiveData<KategoriBmi?>()
 
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
         val tinggiCm = tinggi.toFloat() / 100
@@ -28,5 +31,12 @@ class HitungViewModel : ViewModel(){
         }
         hasilBmi.value = HasilBmi(bmi, kategori)
     }
+    fun mulaiNavigasi() {
+        navigasi.value = hasilBmi.value?.kategori
+    }
+    fun selesaiNavigasi() {
+        navigasi.value = null
+    }
     fun getHasilBmi() : LiveData<HasilBmi?> = hasilBmi
+    fun getNavigasi() : LiveData<KategoriBmi?> = navigasi
 }
